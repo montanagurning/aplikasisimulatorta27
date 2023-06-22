014628bd131d01aabbc9b14c0595e71a13b10bd8
Montana Gurning <montanagurning913@gmailcom>
2023-06-22 21:04:40 +0700

    add something

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from streamlit_lottie import st_lottie
 
 # Load the pre-trained model
 #model = load_model('best_model.h5')
-model = load_model('GRU_60_40_best_model.h5')
+model = load_model('GRU_50_50_best_model.h5')
 
 # Preprocess the dataset
 relevant_columns = ['Open (Rp)', 'High (Rp)', 'Low (Rp)', 'Volume', 'Sentiment (-1/0/1)', 'Close (Rp)']  # Ganti dengan kolom-kolom yang relevan dari dataset Anda
